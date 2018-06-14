dbea3cac856765201ca3064115df0a957848623c
Thomas <pixelman25@gmail.com>
2018-06-14 11:26:18 +0200

    Added rewrites for attributes and cases for map.js

--- views/attribute/map.js
+++ views/attribute/map.js
@@ -1,9 +1,60 @@
 function (o) {
-  for each (c in o['DC.creator']) {
-    var i = c.indexOf(" ");
-    emit(["creator", c.slice(i+1), c.slice(0,i)]);
-  }
-  if (o['DC.issued']) {
-    emit(["issued", o['DC.issued']]);
+  if(o._attachments){
+    for each (c in o['DC.creator']) {
+      var i = c.indexOf(" ");
+      emit(["creator", c.slice(i+1), c.slice(0,i)]);
+    }
+
+    if (o['DC.issued']) {
+      emit(["issued", o['DC.issued']]);
+    }
+
+    if (o['DC.title']) {
+      emit(["title", o['DC.title']]);
+    }
+
+    if (o['DC.relation.ispartof']) {
+      emit(["relation.ispartof", o['DC.relation.ispartof']]);
+    }
+
+    if (o['DC.citation.volume']) {
+      emit(["citation.volume", o['DC.citation.volume']]);
+    }
+
+    if (o['DC.citation.issue']) {
+      emit(["citation.issue", o['DC.citation.issue']]);
+    }
+
+    if (o['DC.citation.spage']) {
+      emit(["citation.spage", o['DC.citation.spage']]);
+    }
+
+    if (o['DC.citation.epage']) {
+      emit(["citation.epage", o['DC.citation.epage']]);
+    }
+
+    if (o['DC.publisher']) {
+      emit(["publisher", o['DC.publisher']]);
+    }
+
+    if (o['url']) {
+      emit(["url", o['url']]);
+    }
+
+    for each (c in o['DC.indexed']) {
+      emit(["indexed", c['indexed']]);
+    }
+
+    if (o['aeresType']) {
+      emit(["aeresType", o['aeresType']]);
+    }
+
+    if (o['abstract']) {
+      emit(["abstract", o['abstract']]);
+    }
+
+    for each (c in o['DC.affiliation']) {
+      emit(["affiliation", o['affiliation']]);
+    }
   }
 }
