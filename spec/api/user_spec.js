45980686c0e447a4d47462a44bf1e3fbc1a49730
Adrian KALMAR <adrian.kalmar@hotmail.com>
2018-06-16 16:49:04 +0200

    TEST: Rewrote the test for service "/user/ID" (see #213).

--- spec/api/user_spec.js
+++ spec/api/user_spec.js
@@ -1,50 +1,16 @@
 const frisby = require('frisby');
 const Joi = frisby.Joi; // Frisby exports Joi for convenience on type assersions
+const V = require('./vhost');
 
-it ('GET should return a status of 200 OK', function () {
-  return frisby
-    .get('http://tireapart.local:5984/attribute/UTT/')
-    .expect('status', 200);
-});
+describe('GET /user/ID', function()
+{
+	const uri = V.HOST + '/user/toto';
 
-it('should have a JSON Content-Type header', function () {
-  return frisby.get('http://tireapart.local:5984/attribute/UTT/')
-    .expect('header', 'Content-Type', 'application/json');
-});
-
-it('should not return an error', function () {
-  return frisby.get('https://httpbin.org/headers')
-    // Should not return an error
-    .expectNot('json', { result: 'error' });
-});
-
-it ('GET /attribute/C/ should return an array of keys and a value', function () {
-  return frisby
-    .get('http://tireapart.local:5984/attribute/UTT/')
-    .expect('status', 200)
-    .expect('jsonTypes', 'rows.*', { // Assert *each* object in 'items' array
-      'key': Joi.array().required(),
-      'value': Joi.number().required()
-    });
-});
-
-it ('GET /attribute/C/A/ should return an array of keys and a value', function () {
-  return frisby
-    .get('http://tireapart.local:5984/attribute/UTT/DC.publisher')
-    .expect('status', 200)
-    .expect('jsonTypes', 'rows.*', { // Assert *each* object in 'items' array
-      'key': Joi.array().required(),
-      'value': Joi.number().required()
-    });
-});
-
-it ('GET /attribute/C/A/V should return an id, an array of keys and a value', function () {
-  return frisby
-    .get('http://tireapart.local:5984/attribute/UTT/DC.publisher/ACM')
-    .expect('status', 200)
-    .expect('jsonTypes', 'rows.*', { // Assert *each* object in 'items' array
-      'id': Joi.string().required(),
-      'key': Joi.array().required(),
-      'value': Joi.object().required()
-    });
+	it ('should return an empty \"rows\" array for a wrong user', function () { 
+	  	return frisby 
+	    	.get(uri)
+	    	.expect('status', 200)
+	    	.expect('header', 'Content-Type', 'application/json')
+	   		.expect('json', 'rows', []);
+	});
 });
