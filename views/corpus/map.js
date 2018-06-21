5b9be2a0f5183c719af38365bf7eb071dea18eb5
Adrian KALMAR <adrian.kalmar@hotmail.com>
2018-06-21 11:19:39 +0200

    REFACTORING: Optimized rewrites handling for corpus and user (see #213).

--- views/corpus/map.js
+++ views/corpus/map.js
@@ -6,63 +6,38 @@ function(o) {
       case "_rev":
       case "_attachments":
       case "_deleted_conflicts":
-      case "_conflicts":
-      case "item_corpus":
-      case "item_name":
-      case "thumbnail":
-      case "resource":
-      case "topics":
-      case "highlights": return true;
+      case "_conflicts": return true;
     }
     return false;
   }
 
-  if (o.corpus_name) {
-  	emit ([o.corpus_name], {"name": o.corpus_name});
-
-  	if (o.users) {
-	  	for (var i = 0; i < o.users.length; i++)
-	  		emit([o.corpus_name], {"user": o.users[i]});
-	  }
-  }
-
-  else if(o._attachments){
+  
+   if(o._attachments){
     //item name, thumbnail and resource
     emit(["UTT", o._id], {
-      name:o.item_name,
-      thumbnail:o.thumbnail,
-      resource:o.resource
+      Authors: o["DC.creator"],
+      Title: o["DC.title"],
+      Abstract: o["abstract"],
+      ispartof: o["DC.relation.ispartof"],
+      volume: o["DC.citation.volume"],
+      issue: o["DC.citation.issue"],
+      spage: o["DC.citation.spage"],
+      epage: o["DC.citation.epage"],
+      publisher: o["DC.publisher"],
+      Year: o["DC.issued"],
+      indexed: o.indexed,
+      aeresType: o.aeresType,
+      name: "tata",
+      thumbnail: "yoyo"
     });
-    //item topics
-    for (var t in o.topics) {
-      var viewpoint_id = o.topics[t].viewpoint;
-      emit(["UTT", o._id], {topic:{
-        viewpoint: viewpoint_id,
-        id: t
-      }, _id: viewpoint_id});
-    }
-    //item highlights
-    for (var h in o.highlights) {
-      var highlight = o.highlights[h];
-      emit(
-        ["UTT", o._id, h], {
-          coordinates: highlight.coordinates,
-          topic: {
-            viewpoint: highlight.viewpoint,
-            id: highlight.topic
-          },
-          thumbnail: highlight.thumbnail,
-          text: highlight.text
-        }
-      );
-    }
+
     //item attributes
-    for (var key in o) {
+/*    for (var key in o) {
       if (!isReserved(key)) {
         var entry = {};
         entry[key] = o[key];
         emit(["UTT", o._id], entry);
       }
-    }
+    }*/
   }
 }
--- views/user/map.js
+++ views/user/map.js
@@ -1,6 +1,6 @@
 function(o) {
     if (o.viewpoint_name) {
-      emit(["open-access"], {viewpoint:{id:o._id, name:o.viewpoint_name}, _id:o._id});
+      emit(["open-access"], {viewpoint:{id:o._id, name:o.viewpoint_name}});
     }
 
     else if (o.corpus_name) {
